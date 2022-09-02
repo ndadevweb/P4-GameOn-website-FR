c74fbc18962422a33c23d631871605187702297a
Nicolas D <oc_nicolasd>
2022-09-03 01:05:48 +0200

    #2 Implementer entrees du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,6 +25,61 @@ function launchModal() {
 // close modal event
 modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
+// form registration
+const formSignup = document.querySelector('.form-signup');
+
+formSignup.addEventListener('submit', validate);
+
+
+/**
+ * Close the modal
+ */
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+/**
+ * Check form is valid
+ *
+ * @param {SubmitEvent} event
+ * @return {boolean}
+ */
+function validate(event) {
+
+  const fieldsNotValid = [];
+
+  const first = event.currentTarget?.first?.value;
+  const last = event.currentTarget?.last?.value;
+  const quantity = event.currentTarget?.quantity?.value;
+  const email = event.currentTarget?.email?.value;
+  const location = event.currentTarget?.querySelectorAll('input[name="location"]:checked');
+  const readAndConditionsAccepted = event.currentTarget?.querySelector('#checkbox1')
+
+  if(/^[a-zA-Z]{2,}$/.test(first) === false) {
+    fieldsNotValid.push("first");
+  }
+
+  if(/^[a-zA-Z]{2,}$/.test(last) === false) {
+    fieldsNotValid.push("last");
+  }
+
+  if(/^[a-zA-Z0-9-.+_]+@[a-zA-Z0-9-]+[.]{1}[a-zA-Z]{2,4}$/.test(email) === false) {
+    fieldsNotValid.push("email");
+  }
+
+  if(/^[0-9]?[0-9]$/.test(quantity) === false) {
+    fieldsNotValid.push("quantity");
+  }
+
+  if(location.length === 0) {
+    fieldsNotValid.push("location");
+  }
+
+  if(readAndConditionsAccepted.checked === false) {
+    fieldsNotValid.push("conditions_accepted");
+  }
+
+  if(fieldsNotValid.length !== 0) {
+    event.preventDefault();
+  }
 }
